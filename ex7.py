35e195e7c4ff9d1b30d9d8c4b1050570b578885c
cxy <954830460@qq.com>
2016-02-16 11:49:48 +0800

    ex7: after drills

---
+++ ex7.py
@@ -0,0 +1,20 @@
+print("Mary had a little lamb.")  # 打印字符串
+print("Its fleece was white as %s." % 'snow')  # 打印字符串格式化表达式返回的字符串
+print("And everywhere that Mary went.")  # 打印字符串
+print("." * 10)  # what'd that do?  答：打印10个字符'.'，这里的乘号是重复的意思（操作符重载）
+
+end1 = "C"  # 将字符赋值给变量名
+end2 = "h"  # 将字符赋值给变量名
+end3 = "e"  # 将字符赋值给变量名
+end4 = "e"  # 将字符赋值给变量名
+end5 = "s"  # 将字符赋值给变量名
+end6 = "e"  # 将字符赋值给变量名
+end7 = "B"  # 将字符赋值给变量名
+end8 = "u"  # 将字符赋值给变量名
+end9 = "r"  # 将字符赋值给变量名
+end10 = "g"  # 将字符赋值给变量名
+end11 = "e"  # 将字符赋值给变量名
+end12 = "r"  # 将字符赋值给变量名
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')  # 打印字符合并的结果，传递关键词参数end为空格。
+print(end7 + end8 + end9 + end10 + end11 + end12)  # 打印字符合并的结果
