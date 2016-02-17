925f47f3436964f529f4feec71f12832432c54b6
cxy <954830460@qq.com>
2016-02-17 12:40:14 +0800

    ex16: after study drills

--- ex16.py
+++ ex16.py
@@ -22,12 +22,17 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write('\n')
-target.write(line2)
-target.write('\n')
-target.write(line3)
-target.write('\n')
+# target.write(line1)
+# target.write('\n')
+# target.write(line2)
+# target.write('\n')
+# target.write(line3)
+# target.write('\n')
+target.write("%s\n%s\n%s\n" % (line1, line2, line3))  # study drill 3
 
 print("And finally, we close it.")
 target.close()
+
+# study drill 4, 5
+# 参数'w'表示以可写的模式打开文件，
+# 如果文件中有内容会自动先清除，不需要额外调用truncate方法
---
+++ ex16_studyDrill2.py
@@ -0,0 +1,4 @@
+from sys import argv
+script, filename = argv
+
+print(open(filename).read())
