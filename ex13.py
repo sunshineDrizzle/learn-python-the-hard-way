aecd387141f010c197ea0896c8062b0710edac74
cxy <954830460@qq.com>
2016-02-17 10:27:44 +0800

    ex13: after study drills

--- ex13.py
+++ ex13.py
@@ -6,3 +6,10 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+# study drill 1，2
+# 错误提示我们程序需要在命令行传递四个实参解包给四个变量，不能多也不能少。
+
+# study drill 3
+name = input("What's your name?")
+print("Your name is:", name)
