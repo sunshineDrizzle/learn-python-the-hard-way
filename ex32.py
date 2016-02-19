7ade218604a4ccd5bdede3cabe43e3228776c1b2
cxy <954830460@qq.com>
2016-02-19 10:43:05 +0800

    ex32: after study drills

--- ex32.py
+++ ex32.py
@@ -19,11 +19,22 @@ for i in change:
 elements = []
 
 # then use the range function to do 0 to 5 counts
+'''
 for i in range(0, 6):
     print("Adding %d to the list." % i)
     # append is a function that lists understand
     elements.append(i)
+'''
+# study drill 2 -> assigned range(0,6) directly to elements
+elements.extend(range(0, 6))
 
 # now we can print them out too
 for i in elements:
     print("Element was: %d" % i)
+
+# study drill 1
+# range(0, 6)会返回一个以1为步进的0~5的整数迭代器
+
+# study drill 3
+# 列表自带的方法还有clear（清除列表中的所有元素）；extend（为列表添加来自迭代器的元素）
+# insert（在指定位置插入元素）；reverse（翻转元素顺序）
