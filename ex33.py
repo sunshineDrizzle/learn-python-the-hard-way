4bc0556abb36151e676e467e78ae132bb65dc0e5
cxy <954830460@qq.com>
2016-02-19 11:22:57 +0800

    ex33: after study drills

--- ex33.py
+++ ex33.py
@@ -1,16 +1,23 @@
-i = 0
+# study drill 1, 2, 3, 4
+def append_nums_w(l, top, increment=1):
+    i = 0
+    while i < top:
+        print("At the top i is %d" % i)
+        l.append(i)
+        i += increment
+        print("Numbers in l now: ", l)
+        print("At the bottom i is %d" % i)
+
+
+# study drill 5
+def append_nums_f(l, top, increment=1):
+    for i in range(0, top, increment):
+        l.append(i)
+        print("Numbers in l now: ", l)
+
 numbers = []
-
-while i < 6:
-    print("At the top i is %d" % i)
-    numbers.append(i)
-
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print("At the bottom i is %d" % i)
-
+append_nums_w(numbers, 6, 3)
 
 print("The numbers: ")
-
 for num in numbers:
     print(num)
